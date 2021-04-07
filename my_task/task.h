def3c1aceed1850817b2180eb66005d01da19745
slavaqweqwe <74615552+slavaqweqwe@users.noreply.github.com>
2021-04-07 14:18:38 +0300

    2

---
+++ my_task/task.h
@@ -0,0 +1,21 @@
+﻿// my_sin.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include "vector"
+namespace testspace {
+
+    template <int n>
+    struct Fibonacci {
+        static const int ret = Fibonacci<n - 1>::ret + Fibonacci<n - 2>::ret ;
+    };
+    template <>
+    struct Fibonacci<0> {
+        static const int ret = 0 ;
+    };
+    template <>
+    struct Fibonacci<1> {
+        static const int ret = 1 ;
+    };
+}
---
+++ my_task/tests/tests.cpp
@@ -0,0 +1,28 @@
+﻿// my_sin.cpp: определяет точку входа для приложения.
+//
+
+#include <gtest/gtest.h>
+#include "task.h"
+using namespace testspace;
+TEST(stack_lib_test, ten)
+{
+    int a = Fibonacci<10>::ret;
+    ASSERT_EQ(a, 55);
+}
+TEST(stack_lib_test, zero)
+{
+    int a = Fibonacci<0>::ret;
+    ASSERT_EQ(a, 0);
+}
+TEST(stack_lib_test, one)
+{
+    int a = Fibonacci<1>::ret;
+    ASSERT_EQ(a, 1);
+}
+
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
