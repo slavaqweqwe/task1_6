bc313d8e7e77427386bb2b23a349e45cbd91c3fe
slavaqweqwe <74615552+slavaqweqwe@users.noreply.github.com>
2021-04-13 18:32:37 +0300

    3

--- my_task/task.h
+++ my_task/task.h
@@ -1,21 +1,18 @@
-﻿// my_sin.h : включаемый файл для стандартных системных включаемых файлов
-// или включаемые файлы для конкретного проекта.
-
+﻿
 #pragma once
 
-#include "vector"
 namespace testspace {
 
     template <int n>
     struct Fibonacci {
-        static const int ret = Fibonacci<n - 1>::ret + Fibonacci<n - 2>::ret ;
+        static const int result = Fibonacci<n - 1>::result + Fibonacci<n - 2>::result;
     };
     template <>
     struct Fibonacci<0> {
-        static const int ret = 0 ;
+        static const int result = 0 ;
     };
     template <>
     struct Fibonacci<1> {
-        static const int ret = 1 ;
+        static const int result = 1 ;
     };
 }
--- my_task/tests/tests.cpp
+++ my_task/tests/tests.cpp
@@ -6,17 +6,17 @@
 using namespace testspace;
 TEST(stack_lib_test, ten)
 {
-    int a = Fibonacci<10>::ret;
+    int a = Fibonacci<10>::result;
     ASSERT_EQ(a, 55);
 }
 TEST(stack_lib_test, zero)
 {
-    int a = Fibonacci<0>::ret;
+    int a = Fibonacci<0>::result;
     ASSERT_EQ(a, 0);
 }
 TEST(stack_lib_test, one)
 {
-    int a = Fibonacci<1>::ret;
+    int a = Fibonacci<1>::result;
     ASSERT_EQ(a, 1);
 }
 
